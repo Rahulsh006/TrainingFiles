12c55cd03ec0c1699f34b6a528257707f1aab23d
RAHUL S H <85486874+Rahulsh006@users.noreply.github.com>
2024-09-18 08:59:58 +0530

    Add files via upload

---
+++ Javabasic/src/hey/Main.java
@@ -0,0 +1,14 @@
+package hey;
+
+public class Main {
+	public static void main(String[]args) {
+		Advcalc obj = new Advcalc();
+		int r1= obj.add(9, 10);
+		int r2=obj.sub(7, 5);
+		int r3=obj.mul(3,5);
+		System.out.println(r3);
+		System.out.println(r2);
+		System.out.println(r1);
+		}
+
+}
